23376ba451c31552cde8ac9908c7edefceff2e31
YoonBeomGeun <yunbumkun36@naver.com>
2024-03-25 02:22:49 +0900

    카드 수정

--- 카드.java
+++ 카드.java
@@ -1,10 +1,32 @@
 package Greedy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class 카드 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++) {
+        	int min = 10001;
+        	
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = sc.nextInt();
+				min = Math.min(min, arr[i][j]);
+			}
+			
+			list.add(min);
+		}
+        
+        System.out.println(Collections.max(list));
 
 	}
-
 }
